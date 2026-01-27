b4d31aa8e9a26d156d83ee6c5991804404980954
CoderAyet <alteya75@gmail.com>
2026-01-27 20:03:34 +0800

    Sign in for admin

--- PopCart/src/main.jsx
+++ PopCart/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./auth/AuthContext.jsx";
-import AdminDashboard from "./Admin/Dashboard/Dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
